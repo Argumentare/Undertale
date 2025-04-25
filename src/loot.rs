034097fd5313b24436da3872c560db77c3a8f07d
Argument <168556900+Argumentare@users.noreply.github.com>
2025-04-25 23:32:23 +0300

    e

--- src/loot.rs
+++ src/loot.rs
@@ -2,6 +2,8 @@ use crate::spells::{self, spell};
 use rand::prelude::*;
 use rand::{distr::{Distribution,StandardUniform}};
 use std::any::type_name;
+use std::ffi::NulError;
+use std::ops::{Add, AddAssign};
 #[derive(Debug,PartialEq)]
 pub enum loot
 {
@@ -10,6 +12,7 @@ pub enum loot
     Spell(&'static str),
     HealthPotion,
     ManaPotion,
+    Null,
 }
 
 
@@ -24,20 +27,55 @@ impl Distribution<loot> for StandardUniform {
     }
 }
 
+impl ToString for loot
+{
+    fn to_string(&self) -> String {
+        match self
+        {
+            loot::HealthPotion => "HealthPotion".to_string(),
+            loot::ManaPotion => "ManaPotion".to_string(),
+            loot::Smallcoins(_) => "Smallcoins".to_string(),
+            loot::Spell(_) => "Spell".to_string(),
+            loot::Null => "".to_string(),
+        }
+    }
+}
+
+impl Add for loot
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self,rhs)
+        {
+            (loot::Smallcoins(a),loot::Smallcoins(b)) => loot::Smallcoins(a + b),
+            _ => loot::Null,
+        }
+    }
+}
+
 
 pub fn randomize_loot() -> Vec<loot>
 {
     let mut loot:Vec<loot> = Vec::new();
-    let mut coins:i32 = 0;
+    let mut coins:loot = loot::Smallcoins(0);
     let mut health_potion_c:i32 = 0;
-    let mana_p:loot = loot::ManaPotion;
-    let health_p:loot = loot::HealthPotion;
     let mut mana_potion_c:i32 = 0;
     for n in rand::rng().random_iter::<i32>().take(rand::rng().random_range(1..=4))
     {
         let n = rand::rng().random::<loot>();    
-        
-        
+       
+       
+        match &n.to_string().as_str() {
+            &"HealthPotion" => health_potion_c += 1,
+            &"ManaPotion" => mana_potion_c += 1,
+            &"Smallcoins" => coins = coins + n,
+            &"Spell" => (),
+            &_ => (),
+        }
+        println!("{}",health_potion_c);
+        println!("{}",mana_potion_c);
+        println!("{:?}",coins);
+        loot.push(&n);
     }
     
     
